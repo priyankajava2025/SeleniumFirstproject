3b6f14c23bc0a7cc2e803e3f5c633e7ea65fd0d0
Priyanka <priyanka.java2025@gmail.com>
2025-05-19 09:48:18 +0530

    Hi

--- src/test/java/com/Java/FirstProgram.java
+++ src/test/java/com/Java/FirstProgram.java
@@ -1,10 +1,11 @@
 package com.Java;
 
+
 public class FirstProgram {
 
 	public static void main(String[] args) {
 		
-		System.out.println(First program test");
+		System.out.println("First program test");
 	}
 
 }
