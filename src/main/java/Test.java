61b1e77e4e02de14121908d98449f0085ecc148c
Priyanka <priyanka.java2025@gmail.com>
2025-05-19 07:57:42 +0530

    First Java Program

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,5 @@
 
 public class Test {
+	pulic static void main 
 
 }
---
+++ src/test/java/com/Java/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.Java;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("first program");
+	}
+
+}
